fe8516bdb456f1f1bcfb0104e497b771ce66c17f
karlsminton <ksm1nn0@gmail>
2022-06-23 15:27:41 +0100

    Changed bootstrap to run cron

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-export FLASK_APP=./index.py
-flask run -h 0.0.0.0
+#export FLASK_APP=./index.py
+#flask run -h 0.0.0.0
+cd /home/ubuntu/app
+python3 -m cron.fetch_clubs
+python3 -m cron.fetch_players
